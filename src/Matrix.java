30efb52760e405e2db12972e6b29cfb0f9f1b5e4
KatrinAn <anpipinakaka@gmail.com>
2023-01-02 13:32:25 +0200

    init commit

---
+++ src/Matrix.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Matrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {3, 5, 7, 0},
+                {2, 4, 6, 7},
+                {9, 0, 1, 4}
+                        };
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                System.out.print(matrix[i][j] + " ");
+                        }
+        }
+            System.out.println();
+
+            for (int i =0; i < matrix[0].length; i++ ){
+                for (int j=0; j < 3; j++){
+                    System.out.print(matrix[j][i]+ " ");
+                }
+        }
+    }
+}
+
